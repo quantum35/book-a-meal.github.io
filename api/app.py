4424733ef2f1ace44174c97ab7b435eb0138a70d
quantum35 <quantumcomputing35@aol.com>
2018-04-29 15:36:52 +0300

    Add app.py and its tests

--- api/app.py
+++ api/app.py
@@ -1,8 +1,170 @@
 from flask import Flask, request, jsonify
+from project.myclasses.user import User
+from project.myclasses.caterer import Caterer
+from functools import wraps
+import jwt
 
 app = Flask(__name__)
 
 
+def token_required(f):
+	@wraps(f)
+	def decorated(*args, **kwargs):
+		token = None
 
-if __name__ == '__main__':
-    app.run(port=5000, debug = True)
+		if 'Authorization' in request.headers:
+			token = request.headers['Authorization']
+
+		if not token:
+			return jsonify({"message": "Token is missing!"}), 401
+
+		try:
+			data = jwt.decode(token, '$%SECRET_KEY@#^@$')
+			active = [u for u in User().users if u["user_id"]
+                            == data["user_id"]]
+			current_user = active[0]
+		except:
+			return jsonify({"message": "Token is Invalid!"}), 401
+
+		return f(current_user, *args, **kwargs)
+
+	return decorated
+
+
+@app.route('/api/v1/users', methods=['GET'])
+@token_required
+def get_users(current_user):
+    
+	if not current_user['admin']:
+		return ('You Dont Have Enough Privileges to View userlist @ {}'.format(current_user['username']))
+    
+	users = User().show_users()
+	return jsonify({"message": users})
+
+
+@app.route('/api/v1/auth/signup', methods=['POST'])
+def signup():
+	new_user = User().signup(
+		request.json['username'], request.json['password'], request.json['user_id'])
+	return jsonify({"user": new_user})
+
+
+@app.route('/api/v1/auth/login', methods=['POST'])
+def login():
+	login = User().login(request.json['username'], request.json['password'])
+	return jsonify({"message": login})
+
+
+@app.route('/api/v1/auth/logout', methods=['POST'])
+@token_required
+def logout_user(current_user):
+	logouts = User().logout()
+	return jsonify({"message": logouts})
+
+
+@app.route('/api/v1/meals/', methods=['GET'])
+@token_required
+def get_all_meals(current_user):
+	if not current_user['admin']:
+		return 'Only available for Admin!'
+
+	get_meals = Caterer().get_meals()
+	return jsonify({"messages": get_meals})
+
+
+@app.route('/api/v1/meals/', methods=['POST'])
+@token_required
+def add_meal(current_user):
+	if not current_user['admin']:
+		return 'Only available for Admin!'
+
+	new_meal = Caterer().post_meal(request.json['meal_id'], request.json['meal_name'],
+                                request.json['price'], request.json['category'], request.json['day'])
+	return jsonify({"message": new_meal})
+
+
+@app.route('/api/v1/meals/<mealId>', methods=['PUT'])
+@token_required
+def edit_meal(current_user, mealId):
+	if not current_user['admin']:
+		return 'Only available for Admin!'
+
+	new_meal = Caterer().modify_meal(mealId,
+                                  request.json['meal_name'], request.json['price'], request.json['category'], request.json['day'])
+	return jsonify({"message": new_meal})
+
+
+@app.route('/api/v1/meals/<mealId>', methods=['DELETE'])
+@token_required
+def delete_meals(current_user, mealId):
+	if not current_user['admin']:
+		return 'Only available for Admin!'
+
+	delete_meal = Caterer().delete_ml(mealId)
+	return jsonify({"message": delete_meal})
+
+
+@app.route('/api/v1/menu/', methods=['POST'])
+@token_required
+def setup_menu(current_user):
+	if not current_user['admin']:
+		return 'Only available for Admin!'
+
+	post_menu = Caterer().post_menu(request.json['meal_id'], request.json['meal_name'],
+                                 request.json['price'], request.json['category'], request.json['day'])
+	return jsonify({"messages": post_menu})
+
+
+@app.route('/api/v1/menu/', methods=['GET'])
+@token_required
+def menu_getter(current_user):
+	if current_user['admin']:
+		return 'Only available for users!'
+
+	get_menu = User().get_menu()
+	return jsonify({"messages": get_menu})
+
+
+@app.route('/api/v1/orders', methods=['POST'])
+@token_required
+def make_orders(current_user):
+	if current_user['admin']:
+		return 'Only available for users!'
+
+	new_order = User().make_order(request.json['meal_id'], request.json['meal_name'], request.json['price'],
+                               request.json['category'], request.json['day'], request.json['quantity'], request.json['username'])
+	return jsonify({"messages": new_order})
+
+
+@app.route('/api/v1/orders/<orderId>', methods=['PUT'])
+@token_required
+def modify_orders(current_user, orderId):
+	if current_user['admin']:
+		return 'Only available for users!'
+
+	mod = User().modify_order(orderId, request.json['quantity'])
+	return jsonify({"messages": mod})
+
+
+@app.route('/api/v1/orders/<orderId>', methods=['DELETE'])
+@token_required
+def delete_orders(current_user, orderId):
+	if current_user['admin']:
+		return 'Only available for users!'
+
+	# delete = Caterer().remove_order(orderId)
+	return jsonify({"messages": 'delete'})
+
+
+@app.route('/api/v1/orders', methods=['GET'])
+@token_required
+def get_all_orders(current_user):
+	if not current_user['admin']:
+		return 'Only available for Admin!'
+
+	get_order = Caterer().get_orders()
+	return jsonify({"messages": get_order})
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
--- api/test_endpoints.py
+++ api/test_endpoints.py
@@ -1,1 +1,167 @@
+'''test_api endpoints'''
 import unittest
+import json
+from app import app
+
+
+class TestApiEndpoints(unittest.TestCase):
+    '''class to tests app.py'''
+
+    def setUp(self):
+        '''create a test client'''
+        self.tester = app.test_client()
+        #register user first
+        reg_data = json.dumps(
+            {"user_id": 1, "username": "Queer", "password": "#12345", "admin": False})
+        response = self.tester.post(
+            '/api/v1/auth/signup', data=reg_data, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+        #Login the user
+        log_data = json.dumps({"username": "Queer", "password": "#12345"})
+        response1 = self.tester.post(
+            '/api/v1/auth/login', data=log_data, content_type='application/json')
+        self.assertEqual(response1.status_code, 200)
+
+        self.token = json.loads(response1.data)['message']
+
+        self.headers = {
+            'Authorization': self.token,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        }
+
+    def test_get_users(self):
+        #Test user access to the menu
+        response = self.tester.get('/api/v1/users', headers=self.headers)
+        self.assertEqual(response.status_code, 200)
+
+    #User activities
+
+    def test_user_registration(self):
+        '''Test user registration(POST)'''
+        reg_data = json.dumps(
+            {"user_id": 1, "username": "Queer", "password": "#12345", "admin": False})
+        response = self.tester.post(
+            '/api/v1/auth/signup', data=reg_data, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_login(self):
+        #Test the user login functionality
+        #register user first
+        reg_data = json.dumps(
+            {"user_id": 1, "username": "Queer", "password": "#12345", "admin": False})
+        response = self.tester.post(
+            '/api/v1/auth/signup', data=reg_data, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+        #Login the user
+        log_data = json.dumps({"username": "Queer", "password": "#12345"})
+        response1 = self.tester.post(
+            '/api/v1/auth/login', data=log_data, content_type='application/json')
+        self.assertEqual(response1.status_code, 200)
+
+    def test_user_can_get_menu(self):
+        #Test user access to the menu
+        response = self.tester.get('/api/v1/menu/', headers=self.headers)
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_can_make_an_order(self):
+        #Test the user's order capability
+        meal_data = json.dumps(
+            {"meal_id": 1, "meal_name": "Rice", "price": 200, "category": "dinner", "day": "none"})
+        response = self.tester.post(
+            '/api/v1/meals/', data=meal_data, headers=self.headers)
+        self.assertEqual(response.status_code, 200)
+
+        setup = self.tester.post(
+            '/api/v1/menu/', data=meal_data, headers=self.headers)
+        self.assertEqual(setup.status_code, 200)
+
+        get_menu = self.tester.get('/api/v1/menu/', headers=self.headers)
+        self.assertEqual(get_menu.status_code, 200)
+
+        order_data = json.dumps({"meal_id": 1, "meal_name": "Rice", "price": 200,
+                                 "category": "dinner", "day": "none", "quantity": 1, "username": "ian"})
+
+        make_order = self.tester.post(
+            '/api/v1/orders', data=order_data, headers=self.headers)
+        self.assertEqual(make_order.status_code, 200)
+
+    def test_user_can_modify_an_order(self):
+        #Test the edit order ability
+        meal_data = json.dumps(
+            {"meal_id": 1, "meal_name": "Rice", "price": 200, "category": "dinner", "day": "none"})
+        response = self.tester.post(
+            '/api/v1/meals/', data=meal_data, headers=self.headers)
+        self.assertEqual(response.status_code, 200)
+
+        setup = self.tester.post(
+            '/api/v1/menu/', data=meal_data, headers=self.headers)
+        self.assertEqual(response.status_code, 200)
+
+        order_data = json.dumps({"meal_id": 1, "meal_name": "Rice", "price": 200,
+                                 "category": "dinner", "day": "none", "quantity": 1, "username": "ian"})
+
+        make_order = self.tester.post(
+            '/api/v1/orders', data=order_data, headers=self.headers)
+        self.assertEqual(make_order.status_code, 200)
+
+        mod_order = json.dumps({"quantity": 3})
+        res = self.tester.put('/api/v1/orders/1',
+                              data=mod_order, headers=self.headers)
+        self.assertEqual(res.status_code, 200)
+
+    def test_caterer_get_all_meal_options(self):
+        #test that api can get all books (GET request)
+        response1 = self.tester.get('/api/v1/meals/', headers=self.headers)
+        self.assertEqual(response1.status_code, 200)
+
+    def test_caterer_can_add_a_meal_option(self):
+        meal_data = json.dumps(
+            {"meal_id": 1, "meal_name": "Rice", "price": 200, "category": "dinner", "day": "none"})
+        response = self.tester.post(
+            '/api/v1/meals/', data=meal_data, headers=self.headers)
+        self.assertEqual(response.status_code, 200)
+
+    def test_caterer_can_update_meal_option_by_id(self):
+        meal_data = json.dumps(
+            {"meal_id": 1, "meal_name": "Rice", "price": 200, "category": "dinner", "day": "none"})
+        response = self.tester.post(
+            '/api/v1/meals/', data=meal_data, headers=self.headers)
+        self.assertEqual(response.status_code, 200)
+
+        update_data = json.dumps(
+            {"meal_name": "Meat", "price": 210, "category": "lunch", "day": "Monday"})
+        res = self.tester.put(
+            '/api/v1/meals/1', data=update_data, headers=self.headers)
+        self.assertEqual(res.status_code, 200)
+
+    def test_caterer_can_delete_meal_option_by_id(self):
+        meal_data = json.dumps(
+            {"meal_id": 8, "meal_name": "Rice", "price": 200, "category": "dinner", "day": "none"})
+        response = self.tester.post(
+            '/api/v1/meals/', data=meal_data, headers=self.headers)
+        self.assertEqual(response.status_code, 200)
+
+        res = self.tester.delete('/api/v1/meals/8', headers=self.headers)
+        self.assertEqual(res.status_code, 200)
+
+    def test_caterer_can_setup_menu(self):
+        meal_data = json.dumps(
+            {"meal_id": 1, "meal_name": "Rice", "price": 200, "category": "dinner", "day": "none"})
+        response = self.tester.post(
+            '/api/v1/meals/', data=meal_data, headers=self.headers)
+        self.assertEqual(response.status_code, 200)
+
+        setup = self.tester.post(
+            '/api/v1/menu/', data=meal_data, headers=self.headers)
+        self.assertEqual(setup.status_code, 200)
+
+    def test_caterer_can_get_orders(self):
+        res = self.tester.get('/api/v1/orders', headers=self.headers)
+        self.assertEqual(res.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
