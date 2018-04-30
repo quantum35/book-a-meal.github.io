f8a265fc3dded7adc38d3bda9513d4aab39db754
Adrian Andre <adrianadero.otieno@gmail.com>
2018-04-30 11:37:53 +0300

    [Chore] Removew Cached files

--- API/config.py
+++ API/config.py
@@ -3,3 +3,4 @@ basedir = os.path.abspath(os.path.dirname(__name__))
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_DATABASE_URI = "postgres://localhost:5432"
+# End of config file
