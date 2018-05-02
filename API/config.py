611f73ef72b885dc0f37abd4e60c0c142421ac32
quantum35 <quantumcomputing35@aol.com>
2018-05-02 04:30:44 +0300

    Update travis to increase test coverage

--- API/config.py
+++ API/config.py
@@ -1,6 +1,28 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__name__))
-SQLALCHEMY_ECHO = False
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "postgres://localhost:5432"
-# End of config file
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
