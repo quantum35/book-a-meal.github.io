45afff6a76c6d899e4a1f01baad3c38886d6e49f
quantum35 <quantumcomputing35@aol.com>
2018-05-06 16:48:54 +0300

    [FIX] Add update travis

---
+++ .travis.sh
@@ -0,0 +1,6 @@
+export MODE='testing'
+export FLASK_DEBUG=1
+export FLASK_APP="run.py"
+export SECRET_KEY="jhdjhThe#najahah"
+export APP_SETTINGS="testing"
+export TESTING_DATABASE_URI="postgresql://localhost/testchal_Booka_meal"
---
+++ api/test/.travis.sh
@@ -0,0 +1,5 @@
+export MODE='testing'
+export FLASK_APP='manage.py'
+export FLASK_DEBUG=1
+export SECRET_KEY="key"
+export TestDB_URL="postgres://postgres@localhost/test"
